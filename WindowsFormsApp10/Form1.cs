a98cb518ce169f15773c7375d87d7dd98abdfec1
Alex Borisov <isip_a.a.borisow@mpt.ru>
2021-01-26 14:20:37 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        static IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        static int port = 8888;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TcpListener server = new TcpListener(localAddr, port);
+            server.Start();
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    try
+                    {
+                        if (stream.CanRead)
+                        {
+                            byte[] myReadBuffer = new byte[1024];
+                            StringBuilder myCompleteMessage = new StringBuilder();
+                            int numberOfBytesRead = 0;
+                            do
+                            {
+                                numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+                                myCompleteMessage.AppendFormat("{0}", Encoding.UTF8.GetString(myReadBuffer, 0, numberOfBytesRead));
+                            } while (stream.DataAvailable);
+                            Byte[] responseData = Encoding.UTF8.GetBytes("ZALUPA");
+                            stream.Write(responseData, 0, responseData.Length);
+                        }
+                    }
+                    finally
+                    {
+                        stream.Close();
+                        client.Close();
+                    }
+                }
+                catch
+                {
+                    server.Stop();
+                    break;
+                }
+            }
+        }
+    }
+}
